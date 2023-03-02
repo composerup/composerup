d9d7111d440d9d144f45dda90b6a12e340c05b24
ubabar <ubabar@venturseed.com>
2023-03-03 01:10:40 +0500

    tailwind css installed

---
+++ apps/resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="{{ asset('images/fav.png') }}">
+  <title>Composerup</title>
+  @vite('resources/css/app.css')
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
