7f9a51fe0d504a08c8e7b45903ffc0f00d7a72ed
Jeremy <jeremy@alphabetacreatives.com>
2023-03-11 19:05:49 -0500

    update devops

--- tools/pre_dev_deploy.sh
+++ tools/pre_dev_deploy.sh
@@ -10,9 +10,9 @@ cd composerup || exit
 tar -czf composerup.tgz ./*
 
 gcloud builds submit composerup.tgz \
-  --tag us-central1-docker.pkg.dev/composerup-dev/composerup-dev/composerup-dev1:"$CI_COMMIT_REF_SLUG"
+  --tag us-central1-docker.pkg.dev/composer-up-dev/composerup-dev/composerup-dev1:"$CI_COMMIT_REF_SLUG"
 
 gcloud run deploy composerup-dev1 \
-  --image us-central1-docker.pkg.dev/composerup-dev/composerup-dev/composerup-dev1:"$CI_COMMIT_REF_SLUG" \
+  --image us-central1-docker.pkg.dev/composer-up-dev/composerup-dev/composerup-dev1:"$CI_COMMIT_REF_SLUG" \
   --region us-central1 \
   --service-account cu-dev-cloud-cd@composerup-dev.iam.gserviceaccount.com
--- tools/pre_dev_deploy2.sh
+++ tools/pre_dev_deploy2.sh
@@ -10,9 +10,9 @@ cd apps/wa-api || exit
 tar -czf wa-api.tgz ./*
 
 gcloud builds submit wa-api.tgz \
-  --tag us-central1-docker.pkg.dev/composerup-dev/composerup-dev/composerup-dev2:"$CI_COMMIT_REF_SLUG"
+  --tag us-central1-docker.pkg.dev/composer-up-dev/composerup-dev/composerup-dev2:"$CI_COMMIT_REF_SLUG"
 
 gcloud run deploy wa-api-dev2 \
-  --image us-central1-docker.pkg.dev/composerup-dev/composerup-dev/composerup-dev2:"$CI_COMMIT_REF_SLUG" \
+  --image us-central1-docker.pkg.dev/composer-up-dev/composerup-dev/composerup-dev2:"$CI_COMMIT_REF_SLUG" \
   --region us-central1 \
   --service-account cu-dev-cloud-cd@composerup-dev.iam.gserviceaccount.com
